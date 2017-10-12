07d6d7b9e09e0ff06be0770dd5266634618082a2
yudisidde <kluenueu@ymail.com>
2017-10-12 22:57:12 +0700

    edited v1
    tambah template dan js

--- js/app.js
+++ js/app.js
@@ -1,37 +1,58 @@
-
-$(document).ready(function(){
-	var peopleID=2;
+$(document).ready(function() {
+	var peopleID = 1;
 	getPeople(peopleID);
 });
 
-function getPeople(peopleID){
-	$.getJSON("https://swapi.co/api/people/"+peopleID+"/?format=json", function(data) {
-		var name = data.name;
-		var height = data.height;
-		var mass = data.height;
+function getPeople(peopleID) {
+	var req = $.getJSON("https://swapi.co/api/people/" + peopleID + "/?format=json");
+	req.done(function(data) {
+		var name      = data.name;
+		var height    = data.height;
+		var mass      = data.height;
 		var hairColor = data.hair_color;
 		var skinColor = data.skin_color;
-		var eyeColor = data.eye_color;
+		var eyeColor  = data.eye_color;
 		var birthYear = data.birth_year;
-		var gender = data.gender;
-		var film = [data.films];
-		console.log("Nama = "+name+" Tinggi = "+height+" Berat = "+mass+" Rambut = "+hairColor+" Kulit = "+skinColor+" Mata = "+eyeColor+" TTL = "+birthYear+" Kel = "+gender);
+		var gender    = data.gender;
+		var film      = [data.films];
 		getFilms(film);
+		// biodata(name);
+		$(".konten-main").removeClass("hide");
+	})
+	.fail(function() {
+		console.log( "error" );
+	})
+	.always(function() {
+		// $(".loading").remove();
 	});
 }
 
 
-function getFilms(film){
-	$.each(film, function (i,v) {
-		$.each(v, function (idx,link) {
-			var linkFilm = link+"?format=json";
-			$.getJSON(linkFilm, function(data) {
-				var filmTitle = data.title;
-				var filmOpening = data.opening_crawl;
-				var filmChar = data.characters;
-				$(".hasil").append("<p>"+filmOpening+"</p>")
-			});
+function getFilms(film) {
+	$.each(film, function(i, v) {
+		$.each(v, function(idx, link) {
+			var linkFilm = link + "?format=json";
+				var req = $.getJSON(linkFilm);
+				req.done(function(data) {
+					var filmTitle   = data.title;
+					var filmOpening = data.opening_crawl;
+					var filmChar    = data.characters;
+					biodata(filmOpening);
+				})
+				.fail(function() {
+					console.log( "error" );
+				})
+				.always(function() {
+					$(".loading").remove();
+				});
 		});
 	});
 }
 
+
+function biodata(name){
+	// console.log(name);
+	$(".hasil").append("<p>"+name+"</p>")
+	// $(".biodata-detail .bioheight").text(name);
+	// li.append(bioLeft);
+}

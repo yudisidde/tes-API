3c2529167330f76c1acfb1d7a1570d5a5143670a
yudisidde <kluenueu@ymail.com>
2017-10-13 18:55:03 +0700

    final
    blanja front end engineer test

--- js/app.js
+++ js/app.js
@@ -15,15 +15,19 @@ function getPeople(peopleID) {
 		var birthYear = data.birth_year;
 		var gender    = data.gender;
 		var film      = [data.films];
+		actorProfile(name,height,mass,hairColor,skinColor,eyeColor,birthYear,gender,film);
 		getFilms(film);
-		// biodata(name);
-		$(".konten-main").removeClass("hide");
 	})
 	.fail(function() {
 		console.log( "error" );
 	})
 	.always(function() {
-		// $(".loading").remove();
+		// $(".profile").removeClass("hide");
+
+		setTimeout(function(){
+			$(".loading-page").remove();
+		},2500);
+		
 	});
 }
 
@@ -34,25 +38,64 @@ function getFilms(film) {
 			var linkFilm = link + "?format=json";
 				var req = $.getJSON(linkFilm);
 				req.done(function(data) {
-					var filmTitle   = data.title;
-					var filmOpening = data.opening_crawl;
-					var filmChar    = data.characters;
-					biodata(filmOpening);
+					var filmTitle    = data.title;
+					var filmOpening  = data.opening_crawl;
+					var filmChar     = data.characters;
+					var filmDirector = data.director;
+					var filmProducer = data.producer;
+					var filmRelease  = data.release_date;
+					moviesProfile(filmTitle, filmDirector, filmRelease, filmProducer, filmOpening);
+					console.log(data);
 				})
 				.fail(function() {
 					console.log( "error" );
 				})
 				.always(function() {
-					$(".loading").remove();
+					$(".more-film").removeClass('hide');
+					showMoreFilm();
 				});
 		});
 	});
 }
 
 
-function biodata(name){
-	// console.log(name);
-	$(".hasil").append("<p>"+name+"</p>")
-	// $(".biodata-detail .bioheight").text(name);
-	// li.append(bioLeft);
+function actorProfile(name,height,mass,hairColor,skinColor,eyeColor,birthYear,gender,film){
+	var actorMovie = name.split(" ").splice(-1);
+	$(".actor-name").append(name);
+	$(".actor-height").append(height);
+	$(".actor-mass").append(mass);
+	$(".actor-hair").append(hairColor);
+	$(".actor-skin").append(skinColor);
+	$(".actor-birth").append(birthYear);
+	$(".actor-gender").append(gender);
+	$(".actor-movies").append(actorMovie+" Movie&rsquo;s");
+}
+
+
+function moviesProfile(filmTitle, filmDirector, filmRelease, filmProducer, filmOpening){
+	var tempFilm = $("<div class='col-md-3 col-sm-6 movies-list'><div class='movies-box'><div class='movies-thumb'><div class='overlay'><div class='overlay-icon'><i class='fa fa-play-circle-o'></i></div></div></div><div class='movies-caption'><h3>"+filmTitle+"</h3><div class='movie-credit'><ul><li><span>Director:</span><span>"+filmDirector+"</span></li><li><span>Release:</span><span>"+filmRelease+"</span></li></ul></div></div></div></div>");
+
+	$(".movies-konten .row").append(tempFilm);
+	$(".related-title").empty().append(filmTitle);
+	$(".related-director").empty().append(filmDirector);
+	$(".related-producer").empty().append(filmProducer);
+	$(".related-date").empty().append(filmRelease);
+	$(".related-opening").empty().append(filmOpening);
+}
+
+
+function showMoreFilm(){
+	var myFilm = $(".movies-konten .row").find(".movies-list");
+	var filmLenght = myFilm.length;
+	$(".more-film").removeClass('hide');
+	x = 4;
+	$('.movies-konten .row .movies-list:lt('+x+')').addClass('show');
+	$('.more-film').click(function () {
+		x= (x+5 <= filmLenght) ? x+5 : filmLenght;
+		$('.movies-konten .row .movies-list:lt('+x+')').show();
+	});
+
+	$(".related-more").click(function(){
+		$(".related-caption").toggleClass("open");
+	});
 }
